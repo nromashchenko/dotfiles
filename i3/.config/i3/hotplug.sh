2564f65e40891eea117d9e75f90e2db00f465770
Nikolai Romashchenko <nikolay.romashchenko@gmail.com>
2024-07-30 16:15:19 +0200

    p14s setup updates

--- i3/.config/i3/hotplug.sh
+++ i3/.config/i3/hotplug.sh
@@ -6,10 +6,10 @@ set -e
 HDMI_STATUS=$(</sys/class/drm/card1-HDMI-A-1/status )  
 
 if [ "connected" == "$HDMI_STATUS" ]; then  
-    /usr/bin/xrandr --output HDMI-1-1 --left-of eDP --mode 2560x1440
+    /usr/bin/xrandr --output HDMI-1 --left-of eDP-1 --mode 3840x2160
     /usr/bin/notify-send --urgency=low -t 5000 "Graphics Update" "HDMI plugged in"
 else  
-    /usr/bin/xrandr --output HDMI-1-1 --off
+    /usr/bin/xrandr --output HDMI-1 --off
     /usr/bin/notify-send --urgency=low -t 5000 "Graphics Update" "External monitor disconnected"    
     exit
 fi
---
+++ i3/.config/i3/lock.sh
@@ -0,0 +1,1 @@
+sleep 0.1 && i3lock-fancy-rapid 5 3
